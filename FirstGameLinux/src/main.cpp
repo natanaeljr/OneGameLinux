0dad1fb0b7ff3ec12d389a90eaccd32ec6683e10
Natanael Rabello <natanael.rabello@outlook.com>
2020-12-14 22:40:40 -0300

    Add log system
    Signed-off-by: Natanael Rabello <natanael.rabello@outlook.com>

--- FirstGameLinux/src/main.cpp
+++ FirstGameLinux/src/main.cpp
@@ -8,7 +8,11 @@
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h>
 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+
 #include "firstgame/firstgame.h"
+#include "firstgame/event.h"
 
 using firstgame::FirstGame;
 
@@ -17,14 +21,17 @@ static void processInput(GLFWwindow* window);
 
 int main()
 {
-    printf(">> FirstGameLinux\n");
+    spdlog::set_default_logger(spdlog::stdout_color_mt("main"));
+    spdlog::set_pattern("%Y-%m-%d %T.%e <%^%=5l%$> [%n] %s:%#:%!():  %v");
+    spdlog::set_level(spdlog::level::trace);
+    SPDLOG_INFO("Initializing FirstGameLinux");
 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "FirstGameLinux", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "FirstGameLinux", nullptr, nullptr);
     if (window == nullptr) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -43,14 +50,14 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init();
 
-    std::unique_ptr<FirstGame> firstgame = FirstGame::New();
+    std::unique_ptr<FirstGame> firstgame = FirstGame::New(spdlog::stdout_color_mt("firstgame"));
 
     while (!glfwWindowShouldClose(window)) {
         // input
         processInput(window);
 
         // update & render
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         firstgame->Update(0u);
 
@@ -69,6 +76,8 @@ int main()
         glfwPollEvents();
     }
 
+    SPDLOG_INFO("Terminating FirstGameLinux");
+
     firstgame.reset();
 
     ImGui_ImplOpenGL3_Shutdown();
@@ -76,6 +85,8 @@ int main()
     ImGui::DestroyContext();
 
     glfwTerminate();
+
+    SPDLOG_INFO("Exiting.");
     return 0;
 }
 
